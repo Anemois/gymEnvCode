1bc23784c8765ff5f61d88e83300661ce7c7aef6
Anemois <danieltang1668@gmail.com>
2025-02-26 22:16:31 +0800

    added grabAllyUlt and support for future proofing, fixed grab sp

--- DataGrabber/grab.py
+++ DataGrabber/grab.py
@@ -13,12 +13,30 @@ things to grab:
 '''
 
 class DataGrabber():
-    def __init__(self, chars = ["Feixiao", "Adventurine", "Robin", "March7"]):
+    def __init__(self, chars = ["Feixiao", "Robin", "Adventurine", "March7"]):
+        self.screen = self.screenshot()
+        self.scrX, self.scrY = pg.size()
+        print(self.scrX, self.scrY)
+        self.initImages()
         self.initUltPos()
         self.initSpPos()
         self.cwd = os.getcwd()
-        self.screen = self.screenshot()
-        self.scrX, self.scrY = pg.size()
+
+        self.chars = chars
+        self.ults = [False, False, False, False]
+
+    def initImages(self):
+        self.images = {}
+        cwd = os.getcwd()        
+        directory = os.fsencode(cwd + "/DataGrabber/assets")
+    
+        for file in os.listdir(directory):
+            filename = os.fsdecode(file)
+            if filename.endswith(".png") or filename.endswith(".jpg"): 
+                flname = filename[:-4]
+                self.images[flname] = cv2.imread(directory.decode() + "/" + filename)
+                if(self.images[flname].shape == (1920, 1080)):
+                    self.images[flname] = cv2.resize(self.images[flname], (self.scrX, self.scrY))
 
     def initUltPos(self):
         self.ultPos = [[self.scrX*252//1920, self.scrY*870//1080, self.scrX*50//1920, self.scrY*50//1080], 
@@ -33,17 +51,101 @@ class DataGrabber():
                       [self.scrX*1448//1920, self.scrY*942//1080], [self.scrX*1466//1920, self.scrY*942//1080]]
         self.spColor = (255, 255, 255)
 
+    def showImage(self, img, name = "img"):
+        cv2.imshow(name, img)
+        cv2.waitKey(0)
+
+    def sameColor(self, c1, c2, diff = 0):
+        c = (c1-c2)
+        #print(c)
+        for i in range(len(c)):
+            if(abs(c[i]) > diff):
+                return False
+        return True
+
+    def mse(self, imageA, imageB, xywhA = None, xywhB = None, debug = False):
+        if(xywhA != None):
+            imageA = imageA[xywhA[1]:xywhA[1]+xywhA[3],xywhA[0]:xywhA[0]+xywhA[2]]
+        if(xywhB != None):
+            imageB = imageB[xywhB[1]:xywhB[1]+xywhB[3],xywhB[0]:xywhB[0]+xywhB[2]]
+        if(debug):
+            self.showImage(imageA, "A")
+            self.showImage(imageB, "B")
+
+        #imageA = cv2.cvtColor(imageA, cv2.COLOR_BGR2GRAY)
+        #imageB = cv2.cvtColor(imageB, cv2.COLOR_BGR2GRAY)
+        err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+        err /= float(imageA.shape[0] * imageA.shape[1])
+        return err
+
     def screenshot(self, path = None):
         if(path == None):
             self.screen = cv2.cvtColor(np.array(pg.screenshot()),  cv2.COLOR_RGB2BGR)
         else:
             try:
-                self.screen = cv2.cvtColor(cv2.imread(path),  cv2.COLOR_RGB2BGR)
+                self.screen = cv2.imread(path)
             except:
                 print("Invalid Path Given")
 
-    def grabAllyUlts(self):
-        pass
+    def _grabUltSpace(self):
+        def sv(img, xywh, name):
+            cv2.imwrite(f"{name}.png", img[xywh[1]:xywh[1]+xywh[3],xywh[0]:xywh[0]+xywh[2]])
+        
+        for i, char in enumerate(self.chars):
+            #print(char)
+            if(f"{char}_ult_not_full" in self.images):
+                #self.showImage(self.screen)
+                #self.showImage(self.images[f"{char}_ult_not_full"])
+                sv(self.images[f"{char}_ult_not_full"], self.ultPos[i], f"{char}_ult_not_full")
+            else:
+                i = 0
+                while(f"{char}_ult_not_full_multi_{i}" in self.images):
+                    sv(self.images[f"{char}_ult_not_full_multi_{i}"], self.ultPos[i], f"{char}_ult_not_full_multi_{i}")
+                    print(i)
+                    i += 1
+            if(f"{char}_ult_full" in self.images):
+                sv(self.images[f"{char}_ult_full"], self.ultPos[i], f"{char}_ult_full")
+            else:
+                i = 0
+                while(f"{char}_ult_full_multi_{i}" in self.images):
+                    sv(self.images[f"{char}_ult_full_multi_{i}"], self.ultPos[i], f"{char}_ult_full_multi_{i}")
+                    i += 1
+
+    def grabAllyUlts(self, debug = False):
+        for i, char in enumerate(self.chars):
+            #print(char)
+            mseFull = 10000000
+            mseNotFull = 10000000
+            #print(char)
+            if(f"{char}_ult_not_full" in self.images):
+                #self.showImage(self.screen)
+                #self.showImage(self.images[f"{char}_ult_not_full"])
+                mseNotFull = self.mse(self.screen, self.images[f"{char}_ult_not_full"], xywhA=self.ultPos[i], debug=debug)
+            else:
+                j = 0
+                while(f"{char}_ult_not_full_multi_{j}" in self.images):
+                    mseNotFull = min(mseNotFull, self.mse(self.screen, self.images[f"{char}_ult_not_full_multi_{i}"], xywhA=self.ultPos[i], debug=debug))
+                    #print(i)
+                    j += 1
+            if(f"{char}_ult_full" in self.images):
+                mseFull = self.mse(self.screen, self.images[f"{char}_ult_full"], xywhA=self.ultPos[i], debug=debug)
+            else:
+                j = 0
+                while(f"{char}_ult_full_multi_{j}" in self.images):
+                    mseFull = min(mseNotFull, self.mse(self.screen, self.images[f"{char}_ult_full_multi_{i}"], xywhA=self.ultPos[i], debug=debug))
+                    j += 1
+            #print(char, mseFull, mseNotFull)
+            self.ults[i] = mseFull < mseNotFull
+
+    def grabSp(self):
+        sp = 0
+        for i in range(7):
+            #print(self.screen[self.spPos[i][1]][self.spPos[i][0]] - self.spColor)
+            if(self.sameColor(self.screen[self.spPos[i][1]][self.spPos[i][0]], self.spColor, 3)):
+                sp = i+1
+            else:
+                break
+        return sp
 
     def grabEnemyHp(self):
         pass
@@ -56,3 +158,20 @@ class DataGrabber():
 
     def grabActionOrder(self):
         pass
+
+if __name__ == '__main__':
+    src = DataGrabber()
+
+    def testUltGrab(num = 1):
+        src.screenshot(path=f'C:/Users/ryant/Desktop/Stuff/Dan_stuff/Python/hsrAI/HSRAI/DataGrabber/assets/ultGrabTest/{num}.png')
+        src.grabAllyUlts(debug=True)
+        print(src.ults)
+
+    def testGrab(num = 1):
+        src.screenshot(path=f'C:/Users/ryant/Desktop/Stuff/Dan_stuff/Python/hsrAI/HSRAI/DataGrabber/assets/ultGrabTest/{num}.png')
+        src.grabAllyUlts(debug=False)
+        sp = src.grabSp()
+        print(src.ults, f"sp: {sp}")
+
+    testGrab(num = 1)
+    testGrab(num = 3)
