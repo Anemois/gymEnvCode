a3a600e1a23825dec560c7b28807cc496c874fe6
Anemois <danieltang1668@gmail.com>
2025-03-04 22:44:55 +0800

    changed folder names, because i thought opening up one for mouse movement was too wasting of space, creaetd mouseController.py, prob going to finish it tomorrow and try to integrate everything later(im too lazy today)

---
+++ Interface/mouseController.py
@@ -0,0 +1,23 @@
+import pyautogui as pg
+
+class MouseController():
+    def __init__(self):
+        pass
+
+    def clickBasic(self):
+        pass
+
+    def clickSkill(self):
+        pass
+
+    def clickUlt0(self):
+        pass
+
+    def clickUlt1(self):
+        pass
+
+    def clickUlt2(self):
+        pass
+
+    def clickUlt3(self):
+        pass
